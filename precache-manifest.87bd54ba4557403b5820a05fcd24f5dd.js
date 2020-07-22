73eb7eb7409fe2add8e21e8b571c31d94843bb16
shavarda <example@example.com>
2020-07-22 12:06:35 +0500

    Updates

---
+++ precache-manifest.87bd54ba4557403b5820a05fcd24f5dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d96616b3fba478154111c1989913318",
+    "url": "/react-pages/index.html"
+  },
+  {
+    "revision": "0d324585790c60cdd4c5",
+    "url": "/react-pages/static/css/2.865a12bb.chunk.css"
+  },
+  {
+    "revision": "128e8a958506f193806e",
+    "url": "/react-pages/static/css/main.6aed2be9.chunk.css"
+  },
+  {
+    "revision": "0d324585790c60cdd4c5",
+    "url": "/react-pages/static/js/2.62260f78.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/react-pages/static/js/2.62260f78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "128e8a958506f193806e",
+    "url": "/react-pages/static/js/main.2302a15b.chunk.js"
+  },
+  {
+    "revision": "715c13f5827a9e211060",
+    "url": "/react-pages/static/js/runtime-main.f61ac019.js"
+  }
+]);
